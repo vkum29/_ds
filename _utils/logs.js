e286161bf97522667925a2ac0ca6b3a9b23a9cc1
vkum29 <vkumar76@sapient.com>
2018-05-27 11:19:39 +0530

    [feature]: heapSort

--- _utils/logs.js
+++ _utils/logs.js
@@ -2,7 +2,7 @@ module.exports  = logger = method => {
 	return (...list) => {
 		console.log(' input for method ', ...list);
 		const result = method.apply(null, list);
-		console.log(' output for method ', result);
+		console.log(' output for method ', JSON.stringify(result));
 		return result;
 	}
 };
