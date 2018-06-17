4291cd5d5aa78600c19ce00ad798bb528f989bac
vkum29 <vkumar76@sapient.com>
2018-06-18 03:23:22 +0530

    [feature]: Quick sort

--- _sort/_quick/quick.sort.js
+++ _sort/_quick/quick.sort.js
@@ -10,9 +10,42 @@ const utils = require('../../_utils');
 const {lessOne, plusOne, logger, iterations, swaps} = utils;
 
 // single time running - need to run n times
-const quickSort = (list) => {
-	// TODO: Perform sorting
+let quickSort = (list, left = 0, right = list.length - 1) => {
+	if (left >= right)
+		return list;
+
+	let pivot = getPivot(list, left, right);
+	quickSort(list, left, pivot - 1);
+	quickSort(list, pivot + 1, right);
+
+	return list;
 }
 
+let getPivot = (list, pivot, right) => {
+
+	let left = plusOne(pivot);
+
+	while (right >= left) {
+		if (list[left] >= list[pivot]){
+			if (list[right] <= list[pivot]) {
+				swap(list, left++, right--);
+			} else {
+				right--;
+			}
+		} else {
+			left++;
+		}
+	};
+	swap(list, pivot, right);
+	return right;
+};
+
+let swap = (list, left, right) => {
+	!!(left - right) && ([list[left], list[right]] = [list[right], list[left]]);
+};
+
+const unsortedArray = [12, 1, 34, 39, 6, 10,1];
+quickSort = iterations.increment(quickSort);
+swap = swaps.increment(swap);
 logger(quickSort)([...unsortedArray]);
-console.log(`Total Operations on array of length ${unsortedArray.length} is ${no_of_exexcution}`);
+console.log(`Total Operations on array of length ${unsortedArray.length} is ${iterations.get()}, swaps are is ${swaps.get()}`);
--- _sort/_quick/readme.md
+++ _sort/_quick/readme.md
@@ -1,10 +1,14 @@
 # Quick sort (a.k.a partition sort algorithm) - 1959
-
+Sorting works on comparison, if two item can be compared greater or less than they can be sorted.
 
 ## Quick sort steps
 
+1. Find pivot, this pivot divides the element into two
+2. Sort so that left has smaller value and right has larger values
+3. divide these sorted array of low and high and repeat step 1 and 2 and 3 till it can't be broken.
 
 ## Performance
+
 | Case        | Operations |
 | ----------- | ---------- |
 | Worst       | O(n^2)     |
@@ -13,18 +17,21 @@
 | Auxiliary   | O(1)       |
 
 ## Characteristics
+
 | Type       | Yes/No    |
 | ---------- | --------- |
 | In Place   | Yes       |
 | Stable     | No        |
 | Adaptive   | No        |
 
-
 ### Implementation
+
 [See ./quick.sort.js v1.0](quick.sort.js)
 
 ### QuickSort wiki
+
 [External link to Quick sort wikipedia](https://en.wikipedia.org/wiki/Quicksort)
 
 ### Other Sorting
+
 [See other sorting algorithm](../)
